414a59ac5a142679b26f5d910cc56cd1e6fb0e3f
Vladimir <volx0681@gmail.com>
2025-06-18 17:39:53 +0300

    test_courses

---
+++ PycharmProjects/autotests-ui/Tests/test_courses.py
@@ -0,0 +1,45 @@
+from playwright.sync_api import sync_playwright, expect
+
+def test_empty_courses_list():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+
+        context = browser.new_context()
+
+        page = context.new_page()
+
+        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+        registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+        registration_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+        registration_username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+
+        registration_email_input.fill('user.name@gmail.com')
+        registration_username_input.fill('username')
+        registration_password_input.fill('password')
+
+        registration_button = page.locator('button:has-text("Registration")')
+
+        registration_button.click()
+
+        page.wait_for_url('**/#/dashboard')
+
+        context.storage_state(path="browser-state.json")
+
+        browser = playwright.chromium.launch(headless=False)
+
+        context = browser.new_context(storage_state="browser-state.json")
+
+        page = context.new_page()
+
+        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+
+        page.wait_for_load_state("domcontentloaded")
+
+        courses_heading = page.locator('[data-testid="courses-list-toolbar-title-text"]')
+        expect(courses_heading).to_be_visible()
+
+        no_results_heading = page.locator('[data-testid="courses-list-empty-view-title-text"]')
+        expect(no_results_heading).to_be_visible()
+
+        browser.close()
