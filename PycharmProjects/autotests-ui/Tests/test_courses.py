197eb7b355157bdb46bd08cf929a1c42d20c14af
Vladimir <volx0681@gmail.com>
2025-06-18 17:45:39 +0300

    test

--- PycharmProjects/autotests-ui/Tests/test_courses.py
+++ PycharmProjects/autotests-ui/Tests/test_courses.py
@@ -3,37 +3,32 @@ from playwright.sync_api import sync_playwright, expect
 def test_empty_courses_list():
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch(headless=False)
-
         context = browser.new_context()
-
         page = context.new_page()
 
         page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-        registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        registration_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-        registration_username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+        registration_email_input = page.locator('[data-testid="registration-form-email-input"] input')
+        registration_password_input = page.locator('[data-testid="registration-form-password-input"] input')
+        registration_username_input = page.locator('[data-testid="registration-form-username-input"] input')
 
         registration_email_input.fill('user.name@gmail.com')
         registration_username_input.fill('username')
         registration_password_input.fill('password')
 
-        registration_button = page.locator('button:has-text("Registration")')
-
-        registration_button.click()
+        page.locator('button:has-text("Registration")').click()
 
         page.wait_for_url('**/#/dashboard')
 
         context.storage_state(path="browser-state.json")
 
+        browser.close()
+
         browser = playwright.chromium.launch(headless=False)
-
         context = browser.new_context(storage_state="browser-state.json")
-
         page = context.new_page()
 
         page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
-
         page.wait_for_load_state("domcontentloaded")
 
         courses_heading = page.locator('[data-testid="courses-list-toolbar-title-text"]')
