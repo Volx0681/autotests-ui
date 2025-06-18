3c7110a3aaef67a8bab3d99a32bb80b253f8a9d3
Vladimir <volx0681@gmail.com>
2025-06-18 11:31:40 +0300

    playwright_reg

---
+++ PycharmProjects/playwright_registration.py
@@ -0,0 +1,27 @@
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as playwright:
+
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+    registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    registration_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    registration_username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+
+    registration_email_input.fill('user.name@gmail.com')
+    registration_username_input.fill('username')
+    registration_password_input.fill('password')
+
+    registration_button = page.locator('button:has-text("Registration")')
+    registration_button.click()
+
+    page.wait_for_url('**/#/dashboard')
+
+    dashboard_heading = page.locator('h6[data-testid="dashboard-toolbar-title-text"]')
+
+    expect(dashboard_heading).to_be_visible()
+
+    browser.close()
