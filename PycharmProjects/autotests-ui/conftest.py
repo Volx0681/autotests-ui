720461ef6e1ebf0f9200839fb4b7ebcce02d000d
Vladimir <volx0681@gmail.com>
2025-06-22 21:46:31 +0300

    fixturev2

--- PycharmProjects/autotests-ui/conftest.py
+++ PycharmProjects/autotests-ui/conftest.py
@@ -1,5 +1,6 @@
 import pytest
-from playwright.sync_api import sync_playwright, Page
+from playwright.sync_api import Playwright, Page
+
 
 def pytest_configure(config):
     config.addinivalue_line("markers", "courses: тесты для работы с курсами")
@@ -12,8 +13,10 @@ def pytest_configure(config):
     config.addinivalue_line("markers", "slow: медленные тесты")
 
 @pytest.fixture
-def chromium_page() -> Page:
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        yield browser.new_page()
+def chromium_page(playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    yield page
+    browser.close()
 
