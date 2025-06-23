b7a7eb6fb07f38735d1b134ff08e5cf5fefa7e49
Vladimir <volx0681@gmail.com>
2025-06-23 11:23:46 +0300

    parametrization

--- PycharmProjects/autotests-ui/Tests/test_params.py
+++ PycharmProjects/autotests-ui/Tests/test_params.py
@@ -1,4 +1,5 @@
 import pytest
+from _pytest.fixtures import SubRequest
 
 @pytest.mark.parametrize("number", [1, 2, 3, -1])
 def test_numbers(number: int):
@@ -12,3 +13,32 @@ def test_several_numbers(number: int, expected: int):
 @pytest.mark.parametrize("browser", ["chromium", "webkit", "firefox"])
 def test_multiplication_of_numbers(os: str, browser: str):
     assert len(os + browser) > 0
+
+@pytest.fixture(params=["chromium", "webkit", "firefox"])
+def browser(request):
+    return request.param
+
+def test_open_browser(browser: str):
+    print("running open_browser: {browser}")
+
+@pytest.mark.parametrize("user", ["Alice", "Zara"])
+class test_operations():
+
+    @pytest.mark.parametrize("account", ["credit card", "debit card"])
+    def test_user_withoperations(self, user: str, account: str):
+        print("running test_user_withoperations")
+
+    def test_user_withoutoperations(self, user: str, account: str):
+        print("running test_user_withoutoperations")
+
+
+users { "+74951385921": "rich user" ,
+"+78163254496": "poor user" ,
+"+75132585698": "user withoperations",
+}
+
+@pytest.mark.parametrize("callnumber", ["+74951385921", "+78163254496","+75132585698"],
+                         users.keys(),
+                         ids=lambda phone_number: f" {phone_number}: {users[phone_number]})"
+def test_identifiers(callnumber: str):
+    ...
