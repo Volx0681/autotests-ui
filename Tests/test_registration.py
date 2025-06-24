7f5d4514104ef506777e708ff3357c140ac5829f
Vladimir <volx0681@gmail.com>
2025-06-24 16:53:06 +0300

    markers

--- Tests/test_registration.py
+++ Tests/test_registration.py
@@ -3,6 +3,8 @@ from playwright.sync_api import Page
 from pages.registration_page import RegistrationPage
 from pages.dashboard_page import DashboardPage
 
+@pytest.mark.courses
+@pytest.mark.registration
 def test_successful_registration(page: Page, registration_page: RegistrationPage, dashboard_page: DashboardPage):
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
