72db471494f995907b82a4a5052db55855a7517f
Vladimir <volx0681@gmail.com>
2025-06-29 21:17:35 +0300

    forgotten

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -2,6 +2,7 @@ import pytest
 from playwright.sync_api import Page
 from pages.create_course_page import CreateCoursePage
 from pages.courses_list_page import CoursesListPage
+from pages.dashboard_page import DashboardPage
 
 
 @pytest.fixture
@@ -13,6 +14,12 @@ def create_course_page(chromium_page_with_state: Page) -> CreateCoursePage:
 def courses_list_page(chromium_page_with_state: Page) -> CoursesListPage:
     return CoursesListPage(chromium_page_with_state)
 
+
 @pytest.fixture
 def course_image_file() -> str:
     return "testdata/files/image.png"
+
+
+@pytest.fixture
+def dashboard_page_with_state(chromium_page_with_state: Page) -> DashboardPage:
+    return DashboardPage(page=chromium_page_with_state)
